36c30483c5c2d0f80d8e4c1827eb7563cf119524
Gabriel Morais Barbosa <gabrielmbdals@gmail.com>
2021-12-06 19:21:20 -0300

    Alterando implementação

--- src/br/com/gmbconsultoria/Joquempo.java
+++ src/br/com/gmbconsultoria/Joquempo.java
@@ -8,57 +8,58 @@ public class Joquempo {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int jogador = 0, robo = 0, opcao = 0;
-
+        int pontosJogador = 0, pontosRobo = 0, opcao = 0;
+        System.out.println("Seja bem vindo!");
         while(opcao != 3){
-            System.out.println("Seja bem vindo!");
             System.out.println("Escolha uma opção!");
             System.out.println("1 - Jogar?");
             System.out.println("2 - Pontuação");
             System.out.println("3 - Sair");
+            System.out.println();
             opcao = scanner.nextInt();
-            switch(opcao){
-                case 1:
-                    System.out.println("Vamos jogar?");
-                    System.out.println("Escolha uma das seguintes opções: 1 para Pedra, 2 para Papel ou 3 para tesoura.");
-                    System.out.println("Qual opção você quer? ");
+            if(opcao == 1) {
+                System.out.println("Vamos jogar?");
+                System.out.println("Escolha uma das seguintes opções: 1 para Pedra, 2 para Papel ou 3 para tesoura.");
+                System.out.println("Qual opção você quer? ");
 
-                    int escolhaJogador = scanner.nextInt();
-                    int escolhaRobo = new Random().nextInt(3) + 1;
+                int escolhaJogador = scanner.nextInt();
+                int escolhaRobo = new Random().nextInt(3) + 1;
 
-                    System.out.println("Escolhas: Jogador ("+escolhaJogador+") e Robô ("+escolhaRobo+")");
+                System.out.println();
+                System.out.println("Escolhas: Jogador (" + escolhaJogador + ") e Robô (" + escolhaRobo + ")");
+                System.out.println();
 
-                    if( (escolhaJogador == 3 && escolhaRobo == 2) || (escolhaJogador == 2 && escolhaRobo == 1) || (escolhaJogador == 1 && escolhaRobo == 3)){
-                        jogador++;
-                        System.out.println("Você ganhou parabéns!!!.");
-                    } else if( escolhaJogador == escolhaRobo){
-                        System.out.println("Deu empate!.");
-                    } else {
-                        robo++;
-                        System.out.println("Você perdeu!.");
-                    }
-                    break;
-                case 2:
-                    System.out.println("Pontuação:");
-                    if(jogador == 1){
-                        System.out.println("Jogador - "+ jogador +" ponto.");
-                    }else {
-                        System.out.println("Jogador - "+ jogador +" pontos.");
-                    }
-                    if(robo == 1){
-                        System.out.println("robo - "+ robo +" ponto.");
-                    }else {
-                        System.out.println("robo - "+ robo +" pontos.");
-                    }
-                    break;
-                case 3:
-                    System.out.println("Obrigado por jogar!");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-                    break;
+                if ((escolhaRobo == 3 && escolhaJogador == 2) || (escolhaRobo == 2 && escolhaJogador == 1) || (escolhaRobo == 1 && escolhaJogador == 3)) {
+                    pontosRobo++;
+                    System.out.println("Você perdeu!.");
+                    System.out.println();
+                } else if (escolhaJogador == escolhaRobo) {
+                    System.out.println("Deu empate!.");
+                    System.out.println();
+                } else {
+                    pontosJogador++;
+                    System.out.println("Você ganhou parabéns!!!.");
+                    System.out.println();
+                }
+            } else if (opcao == 2) {
+                System.out.println();
+                System.out.println("Pontuação:");
+                if (pontosJogador == 1) {
+                    System.out.println("Jogador - " + pontosJogador + " ponto.");
+                } else {
+                    System.out.println("Jogador - " + pontosJogador + " pontos.");
+                }
+                if (pontosRobo == 1) {
+                    System.out.println("robo - " + pontosRobo + " ponto.");
+                } else {
+                    System.out.println("robo - " + pontosRobo + " pontos.");
+                }
+            } else if (opcao == 3) {
+                System.out.println("Obrigado por jogar!");
+            } else {
+                System.out.println("Opção inválida!");
             }
-
+            System.out.println();
         }
     }
 }
