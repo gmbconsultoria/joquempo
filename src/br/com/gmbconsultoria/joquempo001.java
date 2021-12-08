f7b0eb8b7791fb8d055f8680b187d8cc3f439a02
vitorchx@outlook.com <vitorchx@outlook.com>
2021-12-08 07:51:09 -0300

    em andamento

---
+++ src/br/com/gmbconsultoria/joquempo001.java
@@ -0,0 +1,41 @@
+package br.com.gmbconsultoria;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int pontosJogador = 0;
+        int pontosRobo = 0;
+        int opcao = 0;
+
+        System.out.println("Seja bem vindo!");
+
+        while(opcao != 3){
+            System.out.println("Seja bem vindo!");
+            System.out.println("Escolha uma opção!");
+            System.out.println("1 - Jogar?");
+            System.out.println("2 - Pontuação");
+            System.out.println("3 - Sair");
+            opcao = scanner.nextint();
+
+            switch (opcao){
+
+                case 1:
+                    System.out.println("Faça sua escolha: 1- Pedra, 2- Papel ou 3- Tesoura.");
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
