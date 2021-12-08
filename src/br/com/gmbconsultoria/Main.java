cd564a293ed5d9b462d9f11b29f8e1494f989f64
vitorchx@outlook.com <vitorchx@outlook.com>
2021-12-08 08:18:46 -0300

    em andamento

--- src/br/com/gmbconsultoria/Main.java
+++ src/br/com/gmbconsultoria/Main.java
@@ -1,7 +1,7 @@
 package br.com.gmbconsultoria;
 
 
-import java.util.Random;
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,17 +12,16 @@ public class Main {
         int pontosRobo = 0;
         int opcao = 0;
 
-        System.out.println("Seja bem vindo!");
 
-        while(opcao != 3){
+        while (opcao != 3) {
             System.out.println("Seja bem vindo!");
             System.out.println("Escolha uma opção!");
             System.out.println("1 - Jogar?");
             System.out.println("2 - Pontuação");
             System.out.println("3 - Sair");
-            opcao = scanner.nextint();
+            opcao = scan.nextInt();
 
-            switch (opcao){
+            switch (opcao) {
 
                 case 1:
                     System.out.println("Faça sua escolha: 1- Pedra, 2- Papel ou 3- Tesoura.");
