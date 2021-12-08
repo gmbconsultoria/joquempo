cb6186009c996f3edcf721ed3a8e7a3d4fb48da9
vitorchx@outlook.com <vitorchx@outlook.com>
2021-12-08 08:56:04 -0300

    Versão Vitor finalizada

--- src/br/com/gmbconsultoria/Main.java
+++ src/br/com/gmbconsultoria/Main.java
@@ -1,7 +1,7 @@
 package br.com.gmbconsultoria;
 
 
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -19,12 +19,50 @@ public class Main {
             System.out.println("1 - Jogar?");
             System.out.println("2 - Pontuação");
             System.out.println("3 - Sair");
+
             opcao = scan.nextInt();
 
             switch (opcao) {
 
                 case 1:
                     System.out.println("Faça sua escolha: 1- Pedra, 2- Papel ou 3- Tesoura.");
+                    int escolhaJogador = scan.nextInt();
+                    int escolhaRobo = new Random().nextInt(3) + 1;
+                    System.out.println("Escolhas: Jogador: (" + escolhaJogador + ") e Robô: (" + escolhaRobo + ")");
+
+                    if ((escolhaJogador == 2 && escolhaRobo == 3) || (escolhaJogador == 1 && escolhaRobo == 2) || (escolhaJogador == 3 && escolhaRobo == 1)) {
+                        pontosRobo++;
+                        System.out.println("Voce perdeu!");
+
+                    } else if (escolhaJogador == escolhaRobo) {
+                        System.out.println("Empate!!");
+                    } else {
+                        pontosJogador++;
+                        System.out.println("Voce ganhou, parabens!!");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Pontuação:");
+                    if (pontosJogador == 1) {
+                        System.out.println("Jogador: " + pontosJogador + " ponto.");
+                    } else {
+                        System.out.println("Jogador: " + pontosJogador + " pontos.");
+                    }
+                    if (pontosRobo == 1) {
+                        System.out.println("Robo: " + pontosRobo + " ponto.");
+                    } else {
+                        System.out.println("Robo: " + pontosRobo + " pontos.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Obrigado por jogar!");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
 
             }
         }
